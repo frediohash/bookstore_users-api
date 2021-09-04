486e7d586ef958b569dd303f3839e91733d53622
fredio.dimyati <fredio.dimyati@dwidasa.com>
2021-09-04 07:03:58 +0700

    Updated

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/frediohash/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/frediohash/bookstore_users-api/utils/errors"
@@ -9,7 +10,8 @@ import (
 )
 
 const (
-	queryInsertUser = ("INSERT INTO users (first_name, last_name, email, date_created) VALUES (?, ?, ?, ?);")
+	queryInsertUser  = ("INSERT INTO users (first_name, last_name, email, date_created) VALUES (?, ?, ?, ?);")
+	indexUniqueEmail = "unique_Email"
 )
 
 // buat map akses database
@@ -41,24 +43,52 @@ func (user *User) Save() *errors.RestErr {
 		return errors.NewInternalServerError(err.Error())
 	}
 	defer stmt.Close()
-	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
-	if err != nil {
-		return errors.NewInternalServerError(
-			fmt.Sprintf("Error when trying to save user: %s", err.Error())
-		)
-	}
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already exist", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exist", user.Id))
-	}
 
-	// now := time.Now()
-	// user.DateCreated = now.Format("2006-01-02T15:04:05Z")
 	user.DateCreated = date_utils.GetNowString()
 
-	usersDB[user.Id] = user
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), "email_UNIQUE") {
+			return errors.NewBadRequestError("email %s already exist")
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+	user.Id = userId
 	return nil
 }
+
+// yang pertama tanpa database
+// func (user *User) Save() *errors.RestErr {
+// 	stmt, err := users_db.Client.Prepare(queryInsertUser)
+// 	if err != nil {
+// 		return errors.NewInternalServerError(err.Error())
+// 	}
+// 	defer stmt.Close()
+// result, err := users_db.Client.Exec(queryInsertUser, user.FirstName, user.LastName, user.Email, user.DateCreated)
+// 	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+// 	if err != nil {
+// 		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+// 	}
+// 	userId, err := insertResult.LastInsertId()
+// 	if err != nil {
+// 		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+// 	}
+// 	current := usersDB[user.Id]
+// 	if current != nil {
+// 		if current.Email == user.Email {
+// 			return errors.NewBadRequestError(fmt.Sprintf("email %s already exist", user.Email))
+// 		}
+// 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exist", user.Id))
+// 	}
+
+// 	// now := time.Now()
+// 	// user.DateCreated = now.Format("2006-01-02T15:04:05Z")
+// 	user.DateCreated = date_utils.GetNowString()
+
+// 	usersDB[user.Id] = user
+// 	return nil
+// }
