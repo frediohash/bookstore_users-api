8845c878a6770bacc6f5743c661a7ebb80a30a47
ona mel <onamel313@gmail.com>
2022-04-21 04:53:52 +0700

    update

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -1,8 +1,10 @@
 package users
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/frediohash/bookstore_users-api/domain/users"
+	"github.com/frediohash/bookstore_users-api/services"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"net/http"
@@ -17,12 +19,24 @@ func CreateUser(c *gin.Context) {
 	fmt.Println(user)
 	bytes, err := ioutil.ReadAll(c.Request.Body)
 	if err != nil {
+		//TODO Handler Error
 		return
 	}
-	fmt.Println(err)
-	fmt.Println(string(bytes))
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		fmt.Println(err.Error())
+		//TODO Handler Json Error
+		return
+	}
+	//fmt.Println(err)
+	//fmt.Println(string(bytes))
+	result, saveError := services.CreateUser(user)
+	if saveError != nil {
+		//TODO User Creation Error
+		return
+	}
+	c.JSON(http.StatusCreated, result)
+	fmt.Println(user)
 	c.String(http.StatusNotImplemented, "implement me")
-
 }
 
 func GetUser(c *gin.Context) {
